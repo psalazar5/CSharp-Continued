713eb31ebd6ffce741736175b6bcbed2c389e68b
Pablo <psalazar5@live.com>
2023-06-09 04:57:35 -0400

    helloworld program

---
+++ HelloWorldExampleproj/HelloWorldExampleproj/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldExampleproj
+{
+    class Program
+    {
+        public static void Main(String[] args) //string[] args // First line is the C# first main method 
+        {
+            Console.WriteLine("Hello World");  //Coding exercise: Add code to the HelloWorld() function that will log the string "Hello World" to the C# console. On udemy though :"System.Console.WriteLine("Hello World");" 
+            System.Diagnostics.Debug.WriteLine("Hello World");
+         
+        }
+    }
+}
