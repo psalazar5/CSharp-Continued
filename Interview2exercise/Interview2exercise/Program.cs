f8e11f2c7b37f165ea0f841f8d379ad4cc4c619d
Pablo <psalazar5@live.com>
2023-06-13 20:10:08 -0400

    Exercise 2

---
+++ Interview2exercise/Interview2exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics; //BigInteger is a type provided by the 'System.Numerics' namespace that can handle arbitrarily large integers w/o using losing precision. 
+namespace Interview2exercise //It dynamically allocates memory to accommodate the size of the integer values being operated on, allowing calculations with extremely large numbers.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Given an array of integers, write a method to sum the elements in the array, knowing that some of the elements may be very large integers.
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1000000000 };
+            BigInteger sum = sumArray.SumofArray(numbers); //BigInteger represents and handles very large integers using the 'BigInteger' type
+            Console.WriteLine("Sum of Array elements: " + sum);
+
+            //Wrote not a method but inside the program.cs
+            int sumtotal = 0;
+            foreach(int item in numbers)
+            {
+                sumtotal += item;
+            }
+            Console.WriteLine(sumtotal); 
+            //------------------------------------------//
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interview2exercise/Interview2exercise/sumArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Interview2exercise
+{
+    class sumArray
+    {
+        public static BigInteger SumofArray(int[] numbers)
+        {
+            BigInteger sum = 0; 
+            foreach(int number in numbers)
+            {
+                sum += number; 
+            }
+            return sum; 
+        }
+    }
+}
