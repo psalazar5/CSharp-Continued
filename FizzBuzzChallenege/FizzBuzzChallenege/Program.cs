05e1284af2fe2dab507e33916795680135ce71c0
Pablo <psalazar5@live.com>
2023-06-13 23:36:19 -0400

    Challenge

---
+++ FizzBuzzChallenege/FizzBuzzChallenege/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzzChallenege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If the number is divisible by 3, print "Fizz" instead.
+            //If the number is divisible by 5, print "Buzz" instead.
+            //If the number is divisible by both 3 and 5, print "FizzBuzz".
+            //Otherwise, simply print the number itself.
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ class Student.cs
@@ -0,0 +1,54 @@
+class Student
+{
+  string FirstName;
+  string LastName;
+  int StudentId;
+  int GradePointAverage;
+
+  string FormatFullName ()
+  {
+    //method to create a properly formatted name from first and last names 
+    return firstName + " " + lastName;
+  }
+
+  Student()
+  {
+    //base (built-in) constructor method
+  }
+
+  Student(int gpa)
+  {
+    //method where you can get GPA when creating a Student
+    GradePointAverage = gpa;
+  }
+
+  Student(string firstName, string lastName)
+  {
+    //method where you can set names when creating a Student
+    FirstName = firstName;
+    LastName = lastName;
+  }
+
+}
+
+//Creating instances
+
+//Using base constructor:
+
+Student student = new Student();
+student.FirstName = "Bob";
+
+//Using "gpa" constructor: 
+
+int startingGpa = 3.2;
+Student student = new Student(startingGpa)
+
+//Using "named" constructor :
+
+string firstName = "Erik";
+string lastName = "Gross";
+Student student = new Student(firstName, lastName);
+
+//Accessing behavior in the class 
+
+string displayName = student.FormatFullName
