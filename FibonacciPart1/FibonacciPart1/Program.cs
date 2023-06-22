f79f2c36fa21856c045ff063eb6dd9378934ec1b
Pablo <psalazar5@live.com>
2023-06-22 16:00:53 -0400

    Fibonacci

--- FibonacciPart1/FibonacciPart1/Program.cs
+++ FibonacciPart1/FibonacciPart1/Program.cs
@@ -10,6 +10,10 @@ namespace FibonacciPart1
             Console.WriteLine("Please provide input value");
             int count = Int32.Parse(Console.ReadLine());
             Console.Write(p1 + " " + p2);
+            
+            //The Fibonacci sequence is a series of numbers where the next term is the sum of the previous two terms 
+            //Formula: fn = f(n-1) + f(n-2)
+            //The first two terms of the Fibonacci sequence is 0 followed by 1 . 0 1 1 2 3 5 8 13 21 34
 
             //for(int i = 2; i < count; i++)
             //{
