356700eaa2c2af0bd4fbaee471316a62fd486982
Pablo <psalazar5@live.com>
2023-07-11 22:30:04 -0400

    Struct Demonstration

---
+++ MoreStruct/MoreStruct/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MoreStruct
+{
+   
+    class Program
+    {
+        interface IRectangle
+        {
+            int GetArea();
+        }
+        struct Rectangle : IRectangle
+        {
+            int width;
+            int height;
+            public Rectangle(int width, int height)
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public int GetArea()
+            {
+                return this.width * this.height;
+            }
+        }
+        static void Main(string[] args)
+        {
+            IRectangle rectangle = new Rectangle(10, 15);
+            int area = rectangle.GetArea();
+            Console.WriteLine($"The area of rectangle is: {area.ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
