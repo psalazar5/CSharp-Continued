ac9d6e88c049171ce0ea6de7ff1b2bc1d5d1186d
Pablo <psalazar5@live.com>
2023-09-01 01:32:09 -0400

    C#

--- MoreStruct/MoreStruct/Program.cs
+++ MoreStruct/MoreStruct/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.Drawing;
+
 
 namespace MoreStruct
 {
+
     class Program
     {
+        MethodParameters.ChangeHeight(rectangle1);
+
         interface IRectangle
         {
             int GetArea();
         }
-        struct Rectangle : IRectangle
+        struct myRectangle : IRectangle 
         {
             double width;
             double height;
            
-            public Rectangle(double width, double height)
+            public myRectangle(double width, double height)
             {
                 this.width = width;
                 this.height = height;
@@ -28,12 +31,15 @@ namespace MoreStruct
         }
         static void Main(string[] args)
         {
-            IRectangle rectangle = new Rectangle(10, 15);
-            var rectangle1 = new Rectangle(200,300);
-            int area_Of_Rectangle = rectangle1.GetArea();
+            IRectangle rectangle = new myRectangle(10, 15);
+
+            var rectangle1 = new myRectangle(200, 300);
+
             int area = rectangle.GetArea();
-            Console.WriteLine($"The area of rectangle is: {area.ToString()}");
-            Console.WriteLine($"The area of rectangle1 is : {area_Of_Rectangle.ToString()}");
+
+
+            Console.WriteLine($"The area of rectangle is: {area}");
+            Console.WriteLine($"The height of rectangle1 is: {} ");
 
             Console.ReadKey();
         }
---
+++ PassingValue-TypeParameters/PassingValue-TypeParameters/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+var rectangle = new Rectangle(200,300);
+Console.WriteLine($"rectangle height: {rectangle.Height}");
+
+MethodParameters.ChangeHeight(ref rectangle);
+Console.WriteLine($"rectangle height: {rectangle.Height}");
+
+public struct Rectangle
+{
+    public Rectangle(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+    public double Height { get; set; }
+    public double Width { get; set; }
+}
+
+public class MethodParameters
+{
+    public static void ChangeHeight(ref Rectangle rectangle)
+    {
+        rectangle.Height = 500;
+        Console.WriteLine($"rectangle height is {rectangle.Height}");
+    }
+}
