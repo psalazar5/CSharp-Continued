2075daf5454c8246b7d79c3b248d5d9f94f836d0
Pablo <psalazar5@live.com>
2023-09-19 23:08:43 -0400

    DataStructures

--- DataStructures&Algorithms/DataStructures&Algorithms/Program.cs
+++ DataStructures&Algorithms/DataStructures&Algorithms/Program.cs
@@ -1,9 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//using DataStructures_Algorithms.ADT;
+//using System.Collections;
 
-using DataStructures_Algorithms.ADT;
-using System.Collections;
+//Stack ts = new Stack();
+//Pokemon squirtle = new Pokemon(); // declaring data struct. a specific pokemon , abstraction / concrete implementation bc its repsentative of all pokemon
 
-Stack ts = new Stack();
-Pokemon squirtle = new Pokemon(); // declaring data struct. a specific pokemon , abstraction / concrete implementation bc its repsentative of all pokemon
+//Arrays 
+//Array Insertions & Deletions 
 
+//Inserting at the end of an array 
+int[] newArray = new int[6];
 
+//Make a var to keep the length b/c .Length is based off capacity and does not track actual index.
+int length = 0;
+
+//This adds data for us
+for (int i = 0; i < 3; i++)
+{
+    //1 is optional 
+    newArray[length] = i;
+    length++;
+}
+
+newArray[length] = 8;
+length++;
