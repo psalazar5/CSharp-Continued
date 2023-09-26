1890d8a988208b6c74b53d4eeb396d2349424aad
Pablo <psalazar5@live.com>
2023-09-26 18:17:59 -0400

    DataStructures&Algorithms

--- DataStructures&Algorithms/DataStructures&Algorithms/Program.cs
+++ DataStructures&Algorithms/DataStructures&Algorithms/Program.cs
@@ -10,13 +10,13 @@
 //Array Insertions & Deletions 
 
 //Inserting at the end of an array 
-//int[] newArray = new int[10];
+//int[] newArray = new int[8];
 
 //Make a var to keep the length b/c .Length is based off capacity and does not track actual index.
 //int length = 0;
 
 //This adds data for us
-//for (int i = 0; i < 10; i++)
+//for (int i = 0; i < 8; i++)
 //{
 //    //1 is optional 
 //    newArray[length] = i + 1;
@@ -28,9 +28,9 @@
 
 //Inserting at start of array
 
-//for (int i = 8; i >= 0; i--)
+//for (int i = 6; i >= 0; i--)
 //{
-//    //this moves over all values
+//    this moves over all values
 //    newArray[i + 1] = newArray[i];
 //}
 
@@ -40,7 +40,7 @@
 
 //Inserting anywhere in array 
 
-//for (int i = 4; i >= 0; i--)
+//for (int i = 6; i >= 2; i--)
 //{
 //    //Shift each element one position to the right 
 //    newArray[i + 1] = newArray[i];
@@ -49,26 +49,30 @@
 
 //int value = 0;
 
+//for(int i = 0; i < length; i++)
+//{
+//    Console.WriteLine(newArray[i]);
+//}
 
 //Deleting from end of array 
 
-int[] newarray = new int[6];
+//int[] newarray = new int[6];
 
-int length = 0;
+//int length = 0;
 
-for (int i = 0; i < 6; i++)
-{
-    newarray[length] = i;
-    length++;
-}
+//for (int i = 0; i < 6; i++)
+//{
+//    newarray[length] = i;
+//    length++;
+//}
 //length--;
 //deleting anywhere from array (delete index 3)
 
-for(int i = 4; i < length; i++)
-{
-    newarray[i - 1] = newarray[i];
-}
-length--;
+//for(int i = 4; i < length; i++)
+//{
+//    newarray[i - 1] = newarray[i];
+//}
+//length--;
 
 //deletes from start of array 
 //for (int i = 1; i < length; i++)
@@ -77,10 +81,10 @@ length--;
 //}
 //length--;
 
-for (int i = 0; i < length; i++)
-{
-    Console.WriteLine(newarray[i]);
-}
+//for (int i = 0; i < length; i++)
+//{
+//    Console.WriteLine(newarray[i]);
+//}
 
 
 
@@ -132,4 +136,25 @@ for (int i = 0; i < length; i++)
 //array[0] = 30;
 //int value = 0;
 
+//delete at index 3
+//int[] Array = new int[10];
+
+//int Length = 0; 
+
+//for(int i = 0; i < 10; i++)
+//{
+//    Array[Length] = i;
+//    Length++;
+//}
+
+//for(int i = 4; i < Length; i++)
+//{
+//    Array[i-1] = Array[i];
+//}
+//Length--;
+
+//for(int i = 0; i < Length; i++)
+//{
+//    Console.WriteLine(Array[i]);
+//}
 
