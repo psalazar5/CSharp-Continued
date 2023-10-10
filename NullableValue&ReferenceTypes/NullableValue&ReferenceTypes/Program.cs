3ce99dcf9502ea6bdb171c1b40a986887a0a6faa
Pablo <psalazar5@live.com>
2023-10-09 22:09:27 -0400

    C#

---
+++ NullableValue&ReferenceTypes/NullableValue&ReferenceTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿//int? age = null;
+
+//if (age.HasValue)
+//{
+//    Console.WriteLine(age.Value);
+//}
+//else { Console.WriteLine("Age does not have a value."); }
+//used for when needing to read data from a data base w/ a column that allows null as a value or for variables to indicate that a value has not been set 
+
+//Nullable reference types 
+
+var person1 = new Person("Jasmine");
+Console.WriteLine(person1.Name);
+
+
+Person? person2 = null;
+if(person2 != null) { Console.WriteLine(person2.Name); }
+
+
+Person? person3 = null;
+Console.WriteLine(person3?.Name);
+Console.WriteLine("The end ");
+
+public class Person
+{
+    public Person(string firstName)
+    {
+        Name = firstName;
+    }
+    public string Name { get; set; }
+}
